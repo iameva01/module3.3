93adba4579f6b5bd44aff22db23c7273c51c4806
iameva01 <evatagirova@gmail.com>
2024-08-03 15:10:22 +0300

    Update module3.3.py

--- module3.3.py
+++ module3.3.py
@@ -1,20 +1,17 @@
 def print_params(a = 1, b = 'строка', c = True):
     print(a, b, c)
-    print(a)
-    print(c)
-    print(b)
-    print(b + b)
-    print()
-    # print_params(b = 25) # работает не корректно?
-    # print_params(a, b, c = [1, 2, 3]) # ?
-print_params()
+print_params(b = 25)
+print_params(c = [1, 2, 3])
+print_params(7, None, False)
+print()
+print_params(b = 25)
+print('Функция print_params(b=25) работает корректно')
+print_params(c = [1, 2, 3])
+print('Функция print_params(c = [1,2,3]) работает корректно')
 
 values_list = [23, False, ([2, 1], 'Yes')]
 print(type(values_list))
 values_dict = {'a': None, 'b': None, 'c': None}
-if len(values_dict) == len(values_list):
-    for key, value in zip(values_dict.keys(), values_list):
-        values_dict[key] = value
 print(values_dict)
 print_params(**values_dict)
 print_params(*values_list)
